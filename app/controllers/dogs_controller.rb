1e4ef46c6a1c069ba1c531ab8f52e3330cadcd20
alexrcenteno <alex.centeno1991@gmail.com>
2023-02-03 12:44:58 -0600

    add new and create actions and added view to new and create actions

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -8,4 +8,18 @@ class DogsController < ApplicationController
     @dog = Dog.find_by(id: params[:id])
     render :show
   end
+
+  def new
+    @dog = Dog.new
+    render :new
+  end
+
+  def create
+    @dog = Dog.create(
+      name: params[:dog][:name],
+      breed: params[:dog][:breed],
+      image: params[:dog][:breed],
+    )
+    redirect_to "/dogs"
+  end
 end
